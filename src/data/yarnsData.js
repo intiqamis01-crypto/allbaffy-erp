1a030627853523e729e5aee137f8aa342d2a68b5
intiqam <intiqam01@gmail.com>
2026-08-12 12:27:43 +0400

    yarn data update

--- src/data/yarnsData.js
+++ src/data/yarnsData.js
@@ -6,7 +6,16 @@ export const yarnsData = [
   { id: "puffy_599", brand: "Alize", series: "Puffy", code: "599", colorName: "Fil Dişi", imageUrl: "https://www.hobitu.com/alize-puffy-599-fil-disi-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-16625-45-O.jpg" },
   { id: "puffy_72", brand: "Alize", series: "Puffy", code: "72", colorName: "Hamam Xalatı", imageUrl: "https://www.hobitu.com/alize-puffy-72-bej-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-32544-55-O.jpg" },
   { id: "puffy_216", brand: "Alize", series: "Puffy", code: "216", colorName: "Sarı", imageUrl: "https://www.hobitu.com/alize-puffy-216-sari-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-25455-37-B.jpg" },
-  // Alize Puffy Color (Bir neçə nümunə)
-  { id: "puffycolor_6234", brand: "Alize", series: "Puffy Color", code: "6234", colorName: "Qarışıq Çəhrayı", imageUrl: "https://ideacdn.net/idea/im/41/myassets/products/1036/alize-puffy-color-bebe-orgu-ipi-6234-karisik-pembe-39492-jpg.jpeg?revision=1704980933" },
-  { id: "puffycolor_6235", brand: "Alize", series: "Puffy Color", code: "6235", colorName: "Qarışıq Mavi", imageUrl: "https://ideacdn.net/idea/im/41/myassets/products/1037/alize-puffy-color-bebe-orgu-ipi-6235-karisik-mavi-39493-jpg.jpeg?revision=1704980936" }
-];
+  { id: "puffy_183", brand: "Alize", series: "Puffy", code: "183", colorName: "Açıq-Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-183-acik-mavi-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-11354-37-K.jpg" },
+   { id: "puffy_112", brand: "Alize", series: "Puffy", code: "112", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-112-mavi-bebe-orgu-iplikleri-alize-35587-57-K.jpg" },
+  { id: "puffy_280", brand: "Alize", series: "Puffy", code: "280", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-280-orta-mavi-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-22909-50-K.jpg" },
+  { id: "puffy_289", brand: "Alize", series: "Puffy", code: "289", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-289-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-16045-45-K.jpg" },
+  { id: "puffy_16", brand: "Alize", series: "Puffy", code: "16", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-16-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-9529-38-K.jpg" },
+  { id: "puffy_287", brand: "Alize", series: "Puffy", code: "287", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-287-turkuaz-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-30810-37-K.jpg" },
+  { id: "puffy_294", brand: "Alize", series: "Puffy", code: "294", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-294-petrol-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-24627-51-K.jpg" },
+  { id: "puffy_646", brand: "Alize", series: "Puffy", code: "646", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-646-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-12870-41-K.jpg" },
+  { id: "puffy_637", brand: "Alize", series: "Puffy", code: "637", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-637-havaci-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-20433-48-K.jpg" },
+  { id: "puffy_215", brand: "Alize", series: "Puffy", code: "215", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-215-lacivert-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-32043-54-K.jpg" },
+  { id: "puffy_984", brand: "Alize", series: "Puffy", code: "984", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-984-parlement-bebe-orgu-iplikleri-alize-34487-56-K.jpg" },
+  { id: "puffy_360", brand: "Alize", series: "Puffy", code: "360", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-360-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-12865-40-K.jpg" },
+  { id: "puffy_58", brand: "Alize", series: "Puffy", code: "58", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-58-lacivert-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-12861-38-K.jpg" },

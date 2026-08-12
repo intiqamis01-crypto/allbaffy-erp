1310edfb182942e8d8fc88d0b30f89cebc4f5d07
intiqam <intiqam01@gmail.com>
2026-08-12 12:29:27 +0400

    fix yarnsData syntax and update deployment

--- src/data/yarnsData.js
+++ src/data/yarnsData.js
@@ -7,7 +7,7 @@ export const yarnsData = [
   { id: "puffy_72", brand: "Alize", series: "Puffy", code: "72", colorName: "Hamam Xalatı", imageUrl: "https://www.hobitu.com/alize-puffy-72-bej-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-32544-55-O.jpg" },
   { id: "puffy_216", brand: "Alize", series: "Puffy", code: "216", colorName: "Sarı", imageUrl: "https://www.hobitu.com/alize-puffy-216-sari-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-25455-37-B.jpg" },
   { id: "puffy_183", brand: "Alize", series: "Puffy", code: "183", colorName: "Açıq-Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-183-acik-mavi-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-11354-37-K.jpg" },
-   { id: "puffy_112", brand: "Alize", series: "Puffy", code: "112", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-112-mavi-bebe-orgu-iplikleri-alize-35587-57-K.jpg" },
+  { id: "puffy_112", brand: "Alize", series: "Puffy", code: "112", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-112-mavi-bebe-orgu-iplikleri-alize-35587-57-K.jpg" },
   { id: "puffy_280", brand: "Alize", series: "Puffy", code: "280", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-280-orta-mavi-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-22909-50-K.jpg" },
   { id: "puffy_289", brand: "Alize", series: "Puffy", code: "289", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-289-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-16045-45-K.jpg" },
   { id: "puffy_16", brand: "Alize", series: "Puffy", code: "16", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-16-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-9529-38-K.jpg" },
@@ -18,4 +18,5 @@ export const yarnsData = [
   { id: "puffy_215", brand: "Alize", series: "Puffy", code: "215", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-215-lacivert-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-32043-54-K.jpg" },
   { id: "puffy_984", brand: "Alize", series: "Puffy", code: "984", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-984-parlement-bebe-orgu-iplikleri-alize-34487-56-K.jpg" },
   { id: "puffy_360", brand: "Alize", series: "Puffy", code: "360", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-360-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-12865-40-K.jpg" },
-  { id: "puffy_58", brand: "Alize", series: "Puffy", code: "58", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-58-lacivert-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-12861-38-K.jpg" },
+  { id: "puffy_58", brand: "Alize", series: "Puffy", code: "58", colorName: "Mavi", imageUrl: "https://www.hobitu.com/alize-puffy-58-lacivert-bebe-orgu-iplikleri-alize-kalinlik-7-super-kalin-12861-38-K.jpg" }
+];
